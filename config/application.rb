9d8817a8d995d09b00ea8e4f3b509ae1d8834231
Dimitri Bosch <dimitribosch@hotmail.com>
2021-02-03 22:40:11 +0100

    Fix hash syntax

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module WagonChatApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
   end
